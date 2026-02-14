fc8902010207df00e32631736463484e2b6fc9b0
hayden.html <75038475+hayden-html@users.noreply.github.com>
2026-02-14 11:53:24 +0000

    created the ui for the closed state field

--- calendar_task/src/components/DeliveryDateInput.tsx
+++ calendar_task/src/components/DeliveryDateInput.tsx
@@ -1,29 +1,34 @@
 import { ReactComponent as Calendar } from "../assets/calendar.svg";
 import { ReactComponent as ChevronRight } from "../assets/chevron-right.svg";
 import { ReactComponent as Van } from "../assets/van.svg";
+import "../styles/deliveryDateInput.sass";
 
 export default function DeliveryDateInput() {
   return (
-    <div className="delivery-input--container">
-      <div className="title--container">
-        <h1 className="title--heading">Choose your delivery day</h1>
-        <p className="title--hint p-1">Delivery is always free</p>
+    <div className="delivery_input">
+      <div className="title">
+        <h1 className="title_heading">Choose your delivery day</h1>
+        <p className="title_hint">Delivery is always free</p>
       </div>
-      <button className="selected-delivery--container">
-        <div className="selected-delivery--date">
-          <p>Fri Feb 13</p>
-          <div className="delivery--hint">
-            <Van aria-label="delivery van" />
-            <p>Earliest delivery</p>
+      <button className="selected_delivery">
+        <div className="selected_delivery__date">
+          <p className="delivery_date">Fri Feb 13</p>
+          <div className="delivery_hint">
+            <Van aria-label="delivery van" className="van_icon" />
+            <p className="">Earliest delivery</p>
           </div>
         </div>
-        <div className="delivery--button">
-          <div className="button--icon">
-            <Calendar aria-label="day of calendar month" />
+        <div className="delivery_change_date">
+          <div className="button_icon">
+            <Calendar
+              aria-label="day of calendar month"
+              className="calendar_icon"
+            />
+            <div className="calendar_text">10</div>
           </div>
-          <div className="button--text">
-            <span>Change</span>
-            <ChevronRight aria-label="arrow" />
+          <div className="button">
+            <span className="button_text">Change</span>
+            <ChevronRight aria-label="arrow" className="chevron" />
           </div>
         </div>
       </button>
